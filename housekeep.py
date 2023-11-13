cc4334c60b31d75aba7806dfe90c08b4d34dadd3
Erica M. ("Loonatic") <l.oony@aol.com>
2023-11-12 18:14:18 -0600

    General: Code cleanup, fix alpha issue with rescaing

--- housekeep.py
+++ housekeep.py
@@ -13,14 +13,24 @@ This modified version was built for managing and optimizing Toontown resources, 
 from __future__ import print_function
 from PIL import Image
 
-
 sizes = [4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]  # po2 sizes
+
+
 # Don't include 2 since panda doesn't like that res
 # Nothing should EVER be considered to be higher than 2048 in res for Toontown.
 
 def get_closest(y):
     """ Return the closest power of 2 in either direction"""
-    return min(sizes, key=lambda x: abs(x - y))
+    return min(sizes, key = lambda x: abs(x - y))
+
+
+def get_po2_res(im):
+    """
+    Get dimensions for width height of the nearest power 2 resolution.
+    """
+    width, height = im.size
+    return get_closest(width), get_closest(height)
+
 
 def checkpo2(im):
     name = im.filename
@@ -35,15 +45,21 @@ def checkpo2(im):
         print("Warning: {} resizing height from {} to --> {}".format(name, height, new_dimY))
     return True
 
-def po2(im):
+
+def po2(im, alpha=True):
     """
     Return a resized image that is a power of 2, modified to ignore
     a need of a threshold, also converts wrt each dimension (ex: 1024x512)
     """
+    if alpha:
+        bands = im.split()
+        bands = [b.resize(get_po2_res(im), Image.BILINEAR) for b in bands]
+        return Image.merge('RGBA', bands)
     width, height = im.size
     new_dimX = get_closest(width)
     new_dimY = get_closest(height)
     return im.resize((new_dimX, new_dimY))
+
 
 # https://pillow.readthedocs.io/en/stable/handbook/concepts.html#concept-modes
 
@@ -52,20 +68,20 @@ def checkColorMode(im):
     name = im.filename
     if not cmode == 'RGB' and not cmode == 'RGBA':
         print("Info: File {} from {} is not RGB/RGBA color mode".format(name, cmode))
-        #im = im.convert('RGB')
+        # im = im.convert('RGB')
 
-def checkICCProfile(im):
-    # https://stackoverflow.com/questions/31865743/pil-pillow-decode-icc-profile-information
-    # https://pillow.readthedocs.io/en/stable/reference/ImageCms.html#PIL.ImageCms.CmsProfile
+
+def grabICCProfile(im, removeIccProfile=True):
     name = im.filename
     icc = im.info.get('icc_profile')
-    if icc is not None:
+    if icc is not None and removeIccProfile:
         print("Warning: {} has icc data, will be removed".format(name))
-        return True
-    return False
+        return None
+    return icc
 
-class Housekeep():
-    def __init__(self, files, opt, dryrun=False):
+
+class Housekeep:
+    def __init__(self, files, opt, dryrun=False, compression=9, removeIccProfile=True):
         """
         Driver code
         Compression ranges from 0 to 9, 0 = no compression and 9 is max,
@@ -75,31 +91,26 @@ class Housekeep():
         opt = Optimize, if enabled will take a long time to finish
         dryrun = If true will only print files that will be affected
         """
-        compression = 9
-        try:
-            for file in files:
-                try:
-                    im = Image.open(file)
-                    ft = im.format.upper()
+        for file in files:
+            try:
+                im = Image.open(file)
+                ft = im.format.upper()
 
-                    #checkColorMode(im)
-                    if ft == "JPEG" or ft == "JPG": # JPGs dont have ICC profiles
-                        if not checkpo2(im):
-                            continue
-                        if not dryrun:
-                            po2(im).save(file, quality=100, subsampling=0)
-                    elif ft == "PNG":
-                            if opt: # Gonna run through them all anyway
-                                po2(im).save(file, icc_profile=None, compress_level=compression, format='PNG', optimize=True)
-                            else:
-                                if not checkICCProfile(im) and not checkpo2(im):
-                                    continue
-                                if not dryrun:
-                                    po2(im).save(file, icc_profile=None, compress_level=compression, format='PNG')
-                    else:
-                        po2(im).save(file)
-                except IOError:
-                    print("IO ERROR: Is file an image? -> ", file)
-        except MemoryError:
-            print("Error: out of memory.")
-
+                if ft == "JPEG" or ft == "JPG":  # JPGs dont have ICC profiles
+                    if not checkpo2(im):
+                        continue
+                    if not dryrun:
+                        po2(im, False).save(file, quality = 100, subsampling = 0)
+                elif ft == "PNG":
+                    if not dryrun:
+                        po2(im).save(
+                            file,
+                            icc_profile = grabICCProfile(im, removeIccProfile = removeIccProfile),
+                            compress_level = compression,
+                            format = 'PNG',
+                            optimize = bool(opt)
+                        )
+                else:
+                    po2(im).save(file)
+            except IOError:
+                print("IO ERROR: Is file an image? -> ", file)
